c567ce2af915d6655026963d6bfe792d0ac476da
yonzhi <1546310237@qq.com>
2017-04-11 09:30:08 +0800

    Update17411

--- WheelViewlibrary/src/main/java/com/wheelview/library/dialog/MyWheelDialog.java
+++ WheelViewlibrary/src/main/java/com/wheelview/library/dialog/MyWheelDialog.java
@@ -323,17 +323,4 @@ public class MyWheelDialog extends Dialog implements OnWheelChangedListener, Vie
         }
     }
 
-    public interface OnWheelClickLitener {
-        /**
-         * @param provinceName 省、自治区名称
-         * @param provinceID   省id
-         * @param cityName     城市名称
-         * @param cityID       城市id
-         * @param countryName  城区、县名称
-         * @param countryID    城区、县id
-         */
-        void onOKClick(String provinceName, String provinceID, String cityName, String cityID, String countryName, String countryID);
-
-        void onCancelClick();
-    }
 }
